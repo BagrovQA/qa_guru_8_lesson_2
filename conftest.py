76e6080a8a4e3e8b8cbcf156981bfa65169083a7
BagrovQA <bagrov.chiron@gmail.com>
2023-09-22 20:32:11 +0300

    добавлены данные

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(autouse=True)
+def open_browser():
+    browser.open("https://www.google.com/")
+
+    yield
+
+    browser.quit()
+
+@pytest.fixture(autouse=True)
+def setting_browser():
+    browser.driver.set_window_size(1920, 1080)
